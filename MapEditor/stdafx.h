7f8f1293d769eb0df971a68e15a42e5c365e1658
OlegTheCat <farenheit466@gmail.com>
2012-02-02 00:32:52 +0200

    README, TODO, libs and headers added.

--- MapEditor/stdafx.h
+++ MapEditor/stdafx.h
@@ -10,10 +10,17 @@
 #include <stdio.h>
 #include <windows.h>
 #include <vector>
-#include <SDL\SDL.h>
-#include <GL\GL.h>
-#include <GL\GLU.h>
-#include <GL\GLUI\glui.h>
+#include "../Include/SDL/SDL.h"
+#include "../Include/gl/GL.h"
+#include "../Include/gl/GLU.h"
+#include "../Include/gl/glui/glui.h"
+#pragma comment(lib,"../Lib/opengl32.lib")
+#pragma comment(lib,"../Lib/glu32.lib")
+#pragma comment(lib,"../Lib/glui.lib")
+#pragma comment(lib,"../Lib/freeglut.lib")
+#pragma comment(lib,"../Lib/SDL.lib")
+#pragma comment(lib,"../Lib/SDLmain.lib")
+
 //#include <tchar.h>
 
 // TODO: Установите здесь ссылки на дополнительные заголовки, требующиеся для программы
