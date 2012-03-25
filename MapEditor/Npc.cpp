56058ecd1b9fc934adc8811e199527896d85240a
Oleg Vovk <wolff3008@gmail.com>
2012-03-25 19:29:29 +0300

    add irrlicht

---
+++ MapEditor/Npc.cpp
@@ -0,0 +1,32 @@
+#include "StdAfx.h"
+#include "Npc.h"
+#include "Universe.h"
+/*
+NPC::NPC(int _id)
+{
+	char path[256];
+	
+	id = _id;
+	name = new char[64];
+	strcpy(name, "Test name");
+
+	sprintf(path, "game/%s/resource/item/%d/info.dat", Universe::instance->game->name, id);
+
+	FILE* f = fopen(path, "rb");
+
+	if (f != NULL)
+	{
+		printf("NPC #%d initializtion succeeded \n", id);
+		//Operations of gathering info
+	}
+	fclose(f);
+}
+*/
+NPC::NPC(std::map<std::string, std::string> strings, std::map<std::string, int> integers) : MapObject(strings, integers)
+{
+	//id = intergers["id"];
+}
+
+NPC::~NPC(void)
+{
+}
