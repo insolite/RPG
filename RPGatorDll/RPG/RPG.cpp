53b9d12e5bb1ad9f4a2abc6df4b47fdcb3f4c1fc
Oleg Vovk <wolff3008@gmail.com>
2012-04-18 19:07:50 +0300

    Efect1

---
+++ RPGatorDll/RPG/RPG.cpp
@@ -0,0 +1,11 @@
+// RPG.cpp: главный файл проекта.
+
+#include "stdafx.h"
+
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+    Console::WriteLine(L"Здравствуй, мир!");
+    return 0;
+}
